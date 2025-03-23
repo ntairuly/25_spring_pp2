aa9119a34a9d7f0a714d44d4c500d86e62c2e50f
Ntairuly <ntairuly@gmail.com>
2025-03-23 23:22:03 +0500

    eighthCommit1

--- Lab8/2.py
+++ Lab8/2.py
@@ -4,6 +4,7 @@ import random
 
 pygame.init()
 
+#arguments
 WIDTH = 600
 HEIGHT = 650
 
@@ -12,18 +13,13 @@ HEIGHT = 600
 font_small = pygame.font.SysFont("Verdana", 20)
 CELL = 30
 
+# draw gird or in russian(сетка)
 def draw_grid():
     for i in range(HEIGHT // CELL):
         for j in range(WIDTH // CELL):
             pygame.draw.rect(screen, colorGRAY, (i * CELL, j * CELL, CELL, CELL), 1)
 
-def draw_grid_chess():
-    colors = [colorWHITE, colorGRAY]
-
-    for i in range(HEIGHT // CELL):
-        for j in range(WIDTH // CELL):
-            pygame.draw.rect(screen, colors[(i + j) % 2], (i * CELL, j * CELL, CELL, CELL))
-
+#classes or "objects"
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -59,13 +55,13 @@ class Snake:
         if self.body[0].y < 0:
             self.body[0].y = HEIGHT // CELL - 1
 
-
+    #draws snake in cell form or transforme it to cell format
     def draw(self):
         head = self.body[0]
         pygame.draw.rect(screen, colorRED, (head.x * CELL, head.y * CELL, CELL, CELL))
         for segment in self.body[1:]:
             pygame.draw.rect(screen, colorYELLOW, (segment.x * CELL, segment.y * CELL, CELL, CELL))
-
+    #help to create 
     def check_collision(self, food):
         global food_count,level,FPS
         head = self.body[0]
@@ -86,10 +82,11 @@ class Snake:
 class Food:
     def __init__(self):
         self.pos = Point(9, 9)
-
+    
+    #draws food in cell form or transforme it to cell format
     def draw(self):
         pygame.draw.rect(screen, colorGREEN, (self.pos.x * CELL, self.pos.y * CELL, CELL, CELL))
-
+    
     def generate_random_pos(self, snake_body):
         while True:
             self.pos.x = random.randint(0, WIDTH // CELL - 1)
@@ -112,6 +109,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #find where are you want to go
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 snake.dx = 1
@@ -127,7 +125,7 @@ while running:
                 snake.dy = -1
 
     screen.fill(colorBLACK)
-
+    # uses previous functions
     draw_grid()
 
     snake.move()
@@ -136,6 +134,7 @@ while running:
     snake.draw()
     food.draw()
     
+    #give us level and score in screen
     level_appear = font_small.render("Level:"+str(level), True, colorGREEN)
     score_appear = font_small.render("Score:"+str(food_count), True, colorGREEN)
     
